9e0f3f284dfd0c9a62eaf2432bcf76bfbf14903f
xiaowei.zeng <xiaowei.zeng@windriver.com>
2019-03-28 17:37:33 +0800

    develop

--- uart.c
+++ uart.c
@@ -7,20 +7,23 @@
 #include     <termios.h>
 #include     <errno.h>
 #include     <getopt.h>
+#include     <string.h>
 
 
 #define UART_DEVICE     "/dev/ttyUSB0"
 #define INVALID_AXIS_VALUE  -1
 #define DEFAULT_TIMEOUT     1500
+#define ACTION_LENGTH       60
 
 
 int uart_id;
 
 
 typedef struct arm_args {
-    int abs_x;
-    int abs_y;
-    int timeout;
+    int  abs_x;
+    int  abs_y;
+    int  timeout;
+    char action[ACTION_LENGTH];
 } ARM_ARGS;
 
 
@@ -31,6 +34,7 @@ static void usage(void)
                     "-x | --x_abs       X-axis\n"
                     "-y | --y_abs       Y-axis\n"
                     "-t | --time        complete time.(ms)\n"
+                    "-a | --action      action data"
                     "-h | --help        help message\n");
 }
 
@@ -40,7 +44,7 @@ void parse_args(int argc, char *argv[], ARM_ARGS * arm_args)
     int index;
     int cmd;
 
-    const char params[] = "x:y:t:h";
+    const char params[] = "x:y:t:s:h";
     const struct option long_options[] =
     {
         {"x_abs",       required_argument, NULL, 'x'},
@@ -79,6 +83,10 @@ void parse_args(int argc, char *argv[], ARM_ARGS * arm_args)
             break;
         case 't':
             arm_args->timeout = atoi(optarg);
+            break;
+        case 's':
+            memcpy(arm_args->action, optarg, sizeof(optarg));
+            break;
         case 'h':
             usage();
             exit(EXIT_SUCCESS);
@@ -107,11 +115,15 @@ int uart_init()
     /* set uart parameters */
 
     tcgetattr(uart_id, &options);
-    options.c_cflag |= (CLOCAL | CREAD);
-    options.c_cflag &= ~CSIZE;
-    options.c_cflag &= ~CRTSCTS;
-    options.c_cflag |= CS8;
+
+    options.c_cflag &= ~PARENB;
     options.c_cflag &= ~CSTOPB;
+    options.c_cflag &= ~CSIZE;
+    options.c_cflag |= CS8;
+
+
+    options.c_cflag |= (CLOCAL | CREAD);
+    options.c_cflag &= ~CRTSCTS;
     options.c_iflag |= IGNPAR;
     options.c_oflag = 0;
     options.c_lflag = 0;
@@ -152,13 +164,53 @@ int uart_send(int fd, char *data, int datalen)
     return 0;
 }
 
+
+int cmd_get(char *cmd, int * param)
+{
+    int i;
+    char cmd_single[10];
+
+    /* validate cmd and param */
+
+    if ((cmd == NULL) || (param == NULL))
+    {
+        fprintf(stderr, "cmd or param pointer is NULL\n");
+        return -1;
+    }
+
+    /* check param */
+
+
+
+
+
+    memset(cmd, 0, ACTION_LENGTH);
+
+    for (i = 1; i < 7; i++)
+    {
+        if (param[i] != -1)
+        {
+            snprintf(cmd_single, sizeof(cmd_single), "#%dP%d", i, param[i]);
+            strcat(cmd, cmd_single);
+        }
+    }
+
+    cmd[strlen(cmd)] = '!';
+    cmd[strlen(cmd)+1] = 0;
+
+    return 0;
+
+}
+
 int main(int argc, char *argv[])
 {
     ARM_ARGS arm_args;
+    char action[ACTION_LENGTH] = "#1P1700#2P1500#3P1700#4P1800#5P1500#6P2000T2000!"
 
     arm_args.abs_x   = INVALID_AXIS_VALUE;
     arm_args.abs_y   = INVALID_AXIS_VALUE;
     arm_args.timeout = DEFAULT_TIMEOUT;
+    arm_args.action[0] = 0;
 
     /* Parse the arguments */
 
@@ -167,10 +219,20 @@ int main(int argc, char *argv[])
     if (uart_init() < 0)
         return -1;
 
+    if (arm_args.action != NULL)
+        uart_send(uart_id, arm_args.action, sizeof(arm_args.action)-1);
+
+    snprintf(action, sizeof(action), "#1P%d#2P%d#3P%d#4P%d#5P%d#6P%dT1500!");
+
+/*
     uart_send(uart_id,
               "#1P1700#2P1500#3P1700#4P1800#5P1500#6P2000T2000!",
-              sizeof("#1P1700#2P1500#3P1700#4P1800#5P1500#6P2000T2000!"));
+              strlen("#1P1700#2P1500#3P1700#4P1800#5P1500#6P2000T2000!"));
 
+*/
+
+
+    //sleep(5);
     close(uart_id);
     return 0;
 
